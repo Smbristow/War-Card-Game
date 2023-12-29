0b1be6b5188f8b4f198806042ab1803c1e5c1b25
smbristow <smbristow@gmail.com>
2023-12-29 11:40:34 -0500

    variables declared

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let playerDeck1, playerDeck2, player1Card, player2Card, warPile, isGameRunning, currentRound, player1Score, player2Score, gameLog, isWar, winner, cardValues, selectedCard
 
 
 /*------------------------ Cached Element References ------------------------*/
