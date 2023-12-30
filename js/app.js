0153cabfa40865ad6275be1694b57b7155210eb4
smbristow <smbristow@gmail.com>
2023-12-30 13:46:46 -0500

    functions for decks

--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
 /*-------------------------------- Constants --------------------------------*/
-const NUM_PLAYERS = 2
-const CARDS_PER_PLAYER = 26
-const CARD_VALUES = {
+const numPlayers = 2
+const cardsPerPlayer = 26
+const cardValues = {
   '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
   'Jack': 11, 'Queen': 12, 'King': 13, 'Ace': 14
 }
-const GAME_MESSAGES = {
-  WELCOME: 'Welcome to the game of War!',
-  PLAYER_WIN: 'Player %d wins this round!',
-  TIE: 'It\'s a tie! Prepare for WAR!',
-  GAME_OVER: 'Game over. Player %d wins the game!'
+const gameMessages = {
+  welcome: 'Welcome to the game of War!',
+  roundWinner: 'Player %d wins this round!',
+  tie: 'It\'s a tie! Prepare for WAR!',
+  gameOver: 'Game over. Player %d wins the game!'
 }
-const WAR_CONDITION = 3; // Number of cards drawn during a war (adjustable as per game rules)
-const MAX_ROUNDS = 1000; // Maximum rounds to avoid infinite loops in case of stalemate
+const warCondition = 2; // Number of cards drawn during a war (adjustable as per game rules)
+const maxRounds = 1000; // Maximum rounds to avoid infinite loops in case of stalemate
 
 
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let playerDeck1, playerDeck2, player1Card, player2Card, warPile, isGameRunning, currentRound, player1Score, player2Score, gameLog, isWar, winner, cardValues, selectedCard
+let playerDeck1, playerDeck2, player1Card, player2Card, warPile, isGameRunning, currentRound, player1Score, player2Score, gameLog, isWar, winner, selectedCard
 
 
 /*------------------------ Cached Element References ------------------------*/
@@ -48,4 +48,28 @@ function shuffleDeck (deck) {
     const j = math.floor(math.random() * (i + 1))
     [deck[i], deck [j] = deck[j], deck[i]]
   }
+}
+
+function dealCards(deck, numPlayers, cardsPerPlayer) {
+  const players = []
+  for (let i = 0; i < numPlayers; i++) {
+    players.push([])
+  }
+  
+  for (let i = 0; i < cardsPerPlayer; i++) {
+    for (let j = 0; j < numPlayers; j++) {
+      players[j].push(deck.pop())
+    }
+  }
+
+  return players; 
+
+}
+
+function startGame(numPlayers, cardsPerPlayer) {
+  const deck = createDeck()
+  shuffleDeck(deck)
+  const players = dealCards(deck, numPlayers, cardsPerPlayer)
+
+  return players
 }
